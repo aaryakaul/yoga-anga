8e5e2b254ea78d8ae8e74e12dc4277f3a073753c
aarya kaul <aaryakaul07@gmail.com>
2022-04-09 02:07:30 +0530

    SEO begining

--- components/Meta.js
+++ components/Meta.js
@@ -9,7 +9,7 @@ const Meta = ({ title, description }) => {
       <title>{title}</title>
       <meta name='description' content={description} />
       <meta property='og:description' content={description} />
-      <link rel='preconnect' href='https://fonts.googleapis.com' />
+      <link rel='preconnect' href='https://fonts.googleapis.com&display=swap' />
       <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
       <link
         href='https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,600;0,700;1,100&display=swap'
